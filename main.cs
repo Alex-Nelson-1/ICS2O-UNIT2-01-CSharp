8b4bb63d29f2b7eb73141a88e4a55d464a621371
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-05 18:35:12 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+// Created by: Alex Nelson
+// Created on: Oct 2022
+//
+// This program displays, "Hello, World!"
+
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    Console.WriteLine ("\nDone");
+  }
+}
